8755e699f38a8f596467d331d477dd64a9680d7f
drastik <jwjoshuawalker@gmail.com>
2013-07-07 04:18:19 -0400

    Handle nested menus. Unlock menu width. Remove bottom border on last li. Add option to ignore on admin/ pages. Other slight visual improvments.
    Lift 'ul' restrictions on responsive_menus_simple.css to allow any element to be affected.
    Refactor responsive_menus_simple.js to handle pages w/ ajax events.
    Add help page & information.

--- js/responsive_menus_simple.js
+++ js/responsive_menus_simple.js
@@ -4,33 +4,59 @@
  */
 (function ($) {
 
-  Drupal.responsive_menus = Drupal.responsive_menus || {};
+  /**
+   * Handle clicks & toggling the menu.
+   */
+  var toggler_click = function() {
+    $(this).parent().toggleClass('responsive-toggled');
+  };
+
+  // Iterate through selectors, check window sizes, add some classes.
   Drupal.behaviors.responsive_menus = {
     attach: function (context, settings) {
       settings.responsive_menus = settings.responsive_menus || {};
-      if (!settings.responsive_menus.css_selectors.length) return;
+      if (!settings.responsive_menus.css_selectors.length) {
+        return;
+      }
       if (!settings.responsive_menus.media_size.length) {
         settings.responsive_menus.media_size = 768;
       }
+      // Handle clicks & toggling.
+      var toggler_class = '';
+      $('.responsive-menus .toggler').unbind('click');
       // Iterate through our selectors.
       $.each(settings.responsive_menus.css_selectors, function(index, value) {
-        var toggler_class = 'responsive-menus-' + index;
-        $(value).addClass("responsive-menus-simple").wrap('<div class="responsive-menus ' + toggler_class + '" />');
-        $('.' + toggler_class).prepend('<span class="toggler">&#9776;</span>');
+        if ($(value).length > 1) {
+          // Handle nested menus.  Make sure we get the first, but not children.
+          $(value).each(function(val_index) {
+            if (!$(this).parents('ul').length) {
+              if (!$(this).hasClass('responsive-menus-simple')) {
+                toggler_class = 'responsive-menus-' + index + '-' + val_index;
+                $(this).addClass('responsive-menus-simple').wrap('<div class="responsive-menus ' + toggler_class + '" />');
+                $('.' + toggler_class).prepend('<span class="toggler">&#9776;</span>');
+              }
+            }
+          });
+        }
+        else {
+          // Single level menus.
+          if (!$(value).hasClass('responsive-menus-simple')) {
+            toggler_class = 'responsive-menus-' + index;
+            $(value).addClass('responsive-menus-simple').wrap('<div class="responsive-menus ' + toggler_class + '" />');
+            $('.' + toggler_class).prepend('<span class="toggler">&#9776;</span>');
+          }
+        }
       });
+      // Re-bind click function.  This handles ajax events.
+      $('.responsive-menus .toggler').bind('click', toggler_click);
       // Adjustable width instead of @media queries.
       if (window.innerWidth < settings.responsive_menus.media_size) {
         $('.responsive-menus').addClass('responsified');
       }
-      // Handle clicks & toggling.
-      $('.responsive-menus .toggler').click(function() {
-        $(this).parent().toggleClass('responsive-toggled');
-      });
       // Handle window resizing.
       $(window).resize(function() {
         if(window.innerWidth > settings.responsive_menus.media_size) {
           $('.responsive-menus').removeClass('responsified');
-          $('.responsive-menus-simple').removeClass('responsive-toggled');
         }
         if (window.innerWidth < settings.responsive_menus.media_size) {
           $('.responsive-menus').addClass('responsified');
@@ -40,4 +66,3 @@
   };
 
 }(jQuery));
-
